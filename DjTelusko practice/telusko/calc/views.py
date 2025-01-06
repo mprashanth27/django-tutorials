185a59487b92425436ac813d3e3cf9e9d547b583
m.prashanth <prashanthmangena@gmail.com>
2025-01-06 02:13:12 -0700

    Adding parameter to display user name dynamically

--- DjTelusko practice/telusko/calc/views.py
+++ DjTelusko practice/telusko/calc/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request): #accepts a request and returns 
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'name':'User'})
