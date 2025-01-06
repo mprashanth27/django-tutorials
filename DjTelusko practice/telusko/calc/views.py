e29c8ee36a095291d3932254fe2baa37b2cb23c7
m.prashanth <prashanthmangena@gmail.com>
2025-01-06 01:54:30 -0700

    Adds code in urls.py, views.py for calc app

--- DjTelusko practice/telusko/calc/views.py
+++ DjTelusko practice/telusko/calc/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(reuest): #accepts a request and returns 
+    return HttpResponse("<h1>Hello World</h1>")
