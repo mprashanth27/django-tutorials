056fc69b329a9df514e1ef5f33ed5c22f7c13553
m.prashanth <prashanthmangena@gmail.com>
2025-03-23 20:53:39 -0700

    Adds code in travello views.py to render index.html

--- DjTelusko practice/telusko/travello/views.py
+++ DjTelusko practice/telusko/travello/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request): #accepts a request and returns 
+    return render(request, 'index.html')
