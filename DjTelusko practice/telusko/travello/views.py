533b42675be73d08f6273cce58f6d4b2e055d61e
m.prashanth <prashanthmangena@gmail.com>
2025-04-09 10:21:52 -0700

    makes bali price dynamic on index.html

--- DjTelusko practice/telusko/travello/views.py
+++ DjTelusko practice/telusko/travello/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request): #accepts a request and returns 
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'price': 700})
