71441b0a006f51f1963c8f2458a268e87feb1273
m.prashanth <prashanthmangena@gmail.com>
2025-04-16 15:36:59 -0700

    makes img parameter of destinations dynamic

--- DjTelusko practice/telusko/travello/views.py
+++ DjTelusko practice/telusko/travello/views.py
@@ -7,16 +7,19 @@ def index(request): #accepts a request and returns
     dest1 = Destination()
     dest1.name = "Mumbai"
     dest1.desc = "The City That Never Sleeps"
+    dest1.img = "destination_1.jpg"
     dest1.price = 700
 
     dest2 = Destination()
     dest2.name = "Indonesia"
     dest2.desc = "Bhinneka Tunggal Ika"
+    dest2.img = "destination_2.jpg"
     dest2.price = 578
 
     dest3 = Destination()
     dest3.name = "San Francisco"
     dest3.desc = "The City that Knows How"
+    dest3.img = "destination_3.jpg"
     dest3.price = 679
 
     return render(request, 'index.html', {'dest1': dest1, 'dest2': dest2, 'dest3': dest3})
