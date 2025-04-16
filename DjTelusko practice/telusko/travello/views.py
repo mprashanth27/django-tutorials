9c3e0a02fd8c000b8f56271e1c473fa75cc065fc
m.prashanth <prashanthmangena@gmail.com>
2025-04-16 12:36:03 -0700

    Makes parameters of Indonesia, San Francisco dynamic

--- DjTelusko practice/telusko/travello/views.py
+++ DjTelusko practice/telusko/travello/views.py
@@ -8,4 +8,15 @@ def index(request): #accepts a request and returns
     dest1.name = "Mumbai"
     dest1.desc = "The City That Never Sleeps"
     dest1.price = 700
-    return render(request, 'index.html', {'dest1': dest1})
+
+    dest2 = Destination()
+    dest2.name = "Indonesia"
+    dest2.desc = "Bhinneka Tunggal Ika"
+    dest2.price = 578
+
+    dest3 = Destination()
+    dest3.name = "San Francisco"
+    dest3.desc = "The City that Knows How"
+    dest3.price = 679
+
+    return render(request, 'index.html', {'dest1': dest1, 'dest2': dest2, 'dest3': dest3})
