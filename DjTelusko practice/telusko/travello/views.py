a908c19abcc1cd14bca0f5d61d38ac2791cfef56
m.prashanth <prashanthmangena@gmail.com>
2025-04-16 12:22:09 -0700

    Makes Bali destination parameters (name, img,desc,price) dynamic

--- DjTelusko practice/telusko/travello/views.py
+++ DjTelusko practice/telusko/travello/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 # Create your views here.
 
 def index(request): #accepts a request and returns 
-    return render(request, 'index.html', {'price': 700})
+    dest1 = Destination()
+    dest1.name = "Mumbai"
+    dest1.desc = "The City That Never Sleeps"
+    dest1.price = 700
+    return render(request, 'index.html', {'dest1': dest1})
