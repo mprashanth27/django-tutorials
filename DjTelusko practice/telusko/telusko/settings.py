802439ff8fc955eaa7afca861f59418bdd63c739
m.prashanth <prashanthmangena@gmail.com>
2025-04-17 14:40:44 -0700

    Creates migration file using makemigrations cmd

--- DjTelusko practice/telusko/telusko/settings.py
+++ DjTelusko practice/telusko/telusko/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
