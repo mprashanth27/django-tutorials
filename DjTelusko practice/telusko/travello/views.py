3985f502d26950f0d751aa3f1c38ac1b7380db1f
m.prashanth <prashanthmangena@gmail.com>
2025-04-17 17:57:45 -0700

    Adds destinations thru Django admin page in DB and fetches dynamically, Removes hard coded destination parameters

--- DjTelusko practice/telusko/travello/views.py
+++ DjTelusko practice/telusko/travello/views.py
@@ -4,26 +4,5 @@ from .models import Destination
 # Create your views here.
 
 def index(request): #accepts a request and returns 
-    dest1 = Destination()
-    dest1.name = "Mumbai"
-    dest1.desc = "The City That Never Sleeps"
-    dest1.img = "destination_1.jpg"
-    dest1.price = 700
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.name = "Indonesia"
-    dest2.desc = "Bhinneka Tunggal Ika"
-    dest2.img = "destination_2.jpg"
-    dest2.price = 578
-    dest2.offer = True
-
-    dest3 = Destination()
-    dest3.name = "San Francisco"
-    dest3.desc = "The City that Knows How"
-    dest3.img = "destination_3.jpg"
-    dest3.price = 679
-    dest3.offer = False
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
     return render(request, 'index.html', {'dests': dests})
