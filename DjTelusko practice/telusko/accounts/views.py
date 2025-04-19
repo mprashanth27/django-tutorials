8452df3bfdf96ea704721c0a59e7fb4a0440e779
m.prashanth <prashanthmangena@gmail.com>
2025-04-19 16:10:31 -0700

    Adds User Login

--- DjTelusko practice/telusko/accounts/views.py
+++ DjTelusko practice/telusko/accounts/views.py
@@ -3,6 +3,21 @@ from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username' ]
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, "Invalid Credentials")
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -21,6 +36,7 @@ def register(request):
                 user = User.objects.create_user(username=username, password=password1, email= email, first_name=first_name, last_name= last_name)
                 user.save()
                 messages.info(request,"user created")
+                return redirect('login')
             return redirect('register')
         else:
             messages.info(request,"password not matching..")
